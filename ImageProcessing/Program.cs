77e3b6e787eae080e7dd734a096062e76de90d8e
lucie <lucie@LAPTOP-K2OFAE3Q>
2022-03-01 18:27:14 +0100

    rot

--- ImageProcessing/Program.cs
+++ ImageProcessing/Program.cs
@@ -10,9 +10,12 @@ namespace ImageProcessing
             MyImage img = new MyImage("Images/coco.bmp");
             img.EffetMiroir(0).FromImageToFile("Images/coco22.bmp");
 
+            img.ToBlackAndWhite().FromImageToFile("Images/cocoB&W.bmp");
+
             MyImage img2 = img.Resized(0.3f);
             img2.FromImageToFile("Images/cocoResized.bmp");
             img.Rotation(90).FromImageToFile("Images/coco24.bmp");
+            img.Rotation(90).FromImageToFile("Images/coco23.bmp");
             //MyImage img1 = new MyImage("Images/coco23.bmp");
             
             Console.ReadKey();
